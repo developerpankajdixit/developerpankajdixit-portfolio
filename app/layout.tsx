acfd8111525ad0a6c1321381b681bcdae09f6e87
Pankaj Dixit <developerpankajdixit@gmail.com>
2026-06-23 23:31:24 +0530

    Merge both Vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import SiteHeader from "./components/SiteHeader";
 import SiteFooter from "./components/SiteFooter";
 import { Toaster } from "react-hot-toast";
-<<<<<<< HEAD
 import { Analytics } from "@vercel/analytics/react";
-=======
 import { SpeedInsights } from "@vercel/speed-insights/next";
->>>>>>> origin/main
 import "./globals.css";
 
 const geistSans = Geist({
@@ -93,11 +90,8 @@ export default function RootLayout({
             },
           }}
         />
-<<<<<<< HEAD
         <Analytics />
-=======
         <SpeedInsights />
->>>>>>> origin/main
       </body>
     </html>
   );
